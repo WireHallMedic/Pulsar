95ba7a85917c83854f1a87d7abc6373542efa6e7
Michael <MichaelWidler@yahoo.com>
2023-09-03 08:40:05 -0700

    Create factory method for MapTiles.

--- Pulsar/Zone/MapTileFactory.java
+++ Pulsar/Zone/MapTileFactory.java
@@ -1,8 +1,14 @@
 package Pulsar.Zone;
 
 import Pulsar.GUI.*;
+import java.awt.*;
 
 public class MapTileFactory implements ZoneConstants, GUIConstants
 {
-
+   public static MapTile getTile(TILE_TYPE baseType, Color fgColor, Color bgColor)
+   {
+      MapTile tile = new MapTile(baseType.iconIndex, fgColor.getRGB(), bgColor.getRGB(), baseType.name, 
+                                 baseType.lowPassable, baseType.highPassable, baseType.transparent);
+      return tile;
+   }
 }
