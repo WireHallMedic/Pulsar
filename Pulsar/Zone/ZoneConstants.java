b23ab7ca218a28a8725ce5cde49a6405406b371a
Michael <MichaelWidler@yahoo.com>
2023-09-03 08:35:56 -0700

    Create basic tile types.

--- Pulsar/Zone/ZoneConstants.java
+++ Pulsar/Zone/ZoneConstants.java
@@ -1,6 +1,28 @@
 package Pulsar.Zone;
 
-public interface ZoneConstants
-{
+import WidlerSuite.*;
 
+public interface ZoneConstants extends CP437
+{
+   public enum TILE_TYPE
+   {
+      CLEAR (MIDDLE_DOT_CHAR, "Clear", true, true, true),
+      HIGH_WALL ('#', "Wall", true, true, true),
+      NULL (' ', "", false, false, false);
+      
+      public int iconIndex;
+      public String name;
+      public boolean lowPassable;
+      public boolean highPassable;
+      public boolean transparent;
+      
+      private TILE_TYPE(int i, String n, boolean lp, boolean hp, boolean t)
+      {
+         iconIndex = i;
+         name = n;
+         lowPassable = lp;
+         highPassable = hp;
+         transparent = t;
+      }
+   }
 }
