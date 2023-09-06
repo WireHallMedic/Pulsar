5c0dcffc813ceec9cc1196a415f31a773d3f4b8f
Michael <MichaelWidler@yahoo.com>
2023-09-06 07:51:16 -0700

    Only redraw message panel when messages change.

--- Pulsar/GUI/MessagePanel.java
+++ Pulsar/GUI/MessagePanel.java
@@ -15,6 +15,7 @@ public class MessagePanel extends InfoPanel
    private static final int HEIGHT_TILES = TERMINAL_HEIGHT_TILES - MAP_HEIGHT_TILES - 3;
    private static final int X_ORIGIN = 1;
    private static final int Y_ORIGIN = TERMINAL_HEIGHT_TILES - HEIGHT_TILES - 1;
+   private static boolean redrawF = false;
    
    private static Vector<String> messageList = getEmptyMessageList();
    private static Vector<Color> messageColorList = getEmptyMessageColorList();
@@ -22,12 +23,7 @@ public class MessagePanel extends InfoPanel
    public MessagePanel(int w, int h, TilePalette tp)
    {
       super(w, h, tp);
-      for(int x = 0; x < WIDTH_TILES; x++)
-      for(int y = 0; y < HEIGHT_TILES; y++)
-      {
-         setTile(x + X_ORIGIN, y + Y_ORIGIN, '.', TERMINAL_FG_COLOR, BG_COLOR);
-      }
-      write(X_ORIGIN + ((WIDTH_TILES - 14) / 2), Y_ORIGIN, "Message Panel", 13, 1);
+      clearMessagePanel();
    }
    
    public static Vector<String> getEmptyMessageList()
@@ -53,19 +49,31 @@ public class MessagePanel extends InfoPanel
       messageColorList.insertElementAt(messageColor, 0);
       messageList.removeElementAt(HEIGHT_TILES);
       messageColorList.removeElementAt(HEIGHT_TILES);
+      redrawF = true;
    }
    
    @Override
    public void actionPerformed(ActionEvent ae)
    {
-      for(int i = 0; i < HEIGHT_TILES; i++)
+      if(redrawF)
       {
-         String msg = messageList.elementAt(i);
-         Color color = messageColorList.elementAt(i);
-         if(msg.length() > WIDTH_TILES)
-            msg = msg.substring(0, WIDTH_TILES);
-         write(X_ORIGIN, Y_ORIGIN + i, msg, color.getRGB(), Color.BLACK.getRGB(), WIDTH_TILES, 1);
+         redrawF = false;
+         for(int i = 0; i < HEIGHT_TILES; i++)
+         {
+            String msg = messageList.elementAt(i);
+            Color color = messageColorList.elementAt(i);
+            if(msg.length() > WIDTH_TILES)
+               msg = msg.substring(0, WIDTH_TILES);
+            write(X_ORIGIN, Y_ORIGIN + i, msg, color.getRGB(), Color.BLACK.getRGB(), WIDTH_TILES, 1);
+         }
       }
       super.actionPerformed(ae);
+   }
+   
+   public static void clearMessagePanel()
+   {
+      messageList = getEmptyMessageList();
+      messageColorList = getEmptyMessageColorList();
+      redrawF = true;
    }
 }
