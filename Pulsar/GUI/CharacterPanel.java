a1c0acf156fda603237825fa67fd3bb1b580ade5
Michael <MichaelWidler@yahoo.com>
2023-09-09 14:26:03 -0700

    Create CharacterPanel.

---
+++ Pulsar/GUI/CharacterPanel.java
@@ -0,0 +1,50 @@
+/*
+Displaying character attributes and gear
+*/
+
+
+package Pulsar.GUI;
+
+import WidlerSuite.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import Pulsar.Actor.*;
+import Pulsar.Gear.*;
+import Pulsar.Engine.*;
+
+
+public class CharacterPanel extends RogueTilePanel implements GUIConstants
+{
+
+   public CharacterPanel()
+   {
+      super(TERMINAL_WIDTH_TILES, TERMINAL_HEIGHT_TILES, RECT_TILE_PALETTE);
+      setSize(50, 50);
+      setBorder();
+   }
+   
+   private void setBorder()
+   {
+      int[][] borderTemplate = new int[TERMINAL_WIDTH_TILES][TERMINAL_HEIGHT_TILES];
+      for(int x = 0; x < TERMINAL_WIDTH_TILES; x++)
+      {
+         borderTemplate[x][0] = 1;
+         borderTemplate[x][TERMINAL_HEIGHT_TILES - 1] = 1;
+      }
+      for(int y = 0; y < TERMINAL_HEIGHT_TILES; y++)
+      {
+         borderTemplate[0][y] = 1;
+         borderTemplate[TERMINAL_WIDTH_TILES - 1][y] = 1;
+      }
+      int[][] borderArr = BorderBuilder.getBorderTiles(borderTemplate);
+      for(int x = 0; x < TERMINAL_WIDTH_TILES; x++)
+      for(int y = 0; y < TERMINAL_HEIGHT_TILES; y++)
+      {
+         if(borderTemplate[x][y] != 0)
+         {
+            setTile(x, y, borderArr[x][y], TERMINAL_FG_COLOR, BG_COLOR);
+         }
+      }
+   }
+}
