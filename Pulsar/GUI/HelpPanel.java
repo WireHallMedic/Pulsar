687dabbad7f47e80e36d501468b68a13658272ae
Michael <MichaelWidler@yahoo.com>
2023-09-11 07:26:25 -0700

    Create help panel.

---
+++ Pulsar/GUI/HelpPanel.java
@@ -0,0 +1,115 @@
+/*
+Displaying character attributes and gear
+*/
+
+
+package Pulsar.GUI;
+
+import WidlerSuite.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import Pulsar.Actor.*;
+import Pulsar.Gear.*;
+import Pulsar.Engine.*;
+
+
+public class HelpPanel extends RogueTilePanel implements GUIConstants
+{
+   public static final int X_ORIGIN = 1;
+   public static final int Y_ORIGIN = 1;
+   public static final int WIDTH_TILES = TERMINAL_WIDTH_TILES - 2;
+   public static final int HEIGHT_TILES = TERMINAL_HEIGHT_TILES - 2;
+   private String[][] pageArray;
+   private int curPageIndex;
+   private int maxPageIndex;
+   
+   
+   public HelpPanel()
+   {
+      super(TERMINAL_WIDTH_TILES, TERMINAL_HEIGHT_TILES, RECT_TILE_PALETTE);
+      setSize(50, 50);
+      setBorder();
+      setBackground(BG_COLOR);
+      pageArray = getPages();
+   }
+   
+   private void setBorder()
+   {
+      int[][] borderTemplate = new int[TERMINAL_WIDTH_TILES][TERMINAL_HEIGHT_TILES];
+      for(int x = 0; x < TERMINAL_WIDTH_TILES; x++)
+      {
+         borderTemplate[x][0] = 1;
+         borderTemplate[x][TERMINAL_HEIGHT_TILES - 1] = 1;
+      }
+      for(int y = 0; y < TERMINAL_HEIGHT_TILES; y++)
+      {
+         borderTemplate[0][y] = 1;
+         borderTemplate[TERMINAL_WIDTH_TILES - 1][y] = 1;
+      }
+      int[][] borderArr = BorderBuilder.getBorderTiles(borderTemplate);
+      for(int x = 0; x < TERMINAL_WIDTH_TILES; x++)
+      for(int y = 0; y < TERMINAL_HEIGHT_TILES; y++)
+      {
+         if(borderTemplate[x][y] != 0)
+         {
+            setTile(x, y, borderArr[x][y], TERMINAL_FG_COLOR, BG_COLOR);
+         }
+      }
+   }
+   
+   public void keyPressed(KeyEvent ke)
+   {
+      switch(ke.getKeyCode())
+      {
+         case KeyEvent.VK_ESCAPE :
+         case KeyEvent.VK_H : InnerPanel.setActivePanel(MainGameBGPanel.class); 
+                              GameEngine.setGameMode(EngineConstants.GameMode.STANDARD);
+                              break;
+      }
+   }
+   
+   @Override
+   public void setVisible(boolean v)
+   {
+      if(v)
+      {
+         curPageIndex = 0;
+         update();
+      }
+      super.setVisible(v);
+   }
+   
+   public void update()
+   {
+      clear();
+   }
+   
+   public void clear()
+   {
+      for(int x = X_ORIGIN; x < WIDTH_TILES; x++)
+      for(int y = Y_ORIGIN; y < HEIGHT_TILES; y++)
+      {
+         setTile(x, y, ' ', TERMINAL_FG_COLOR, BG_COLOR);
+      }
+   }
+   
+   public String[][] getPages()
+   {
+      String[] pageOne = {
+      "Page One",
+      "a"
+      };
+      String[] pageTwo = {
+      "Page Two",
+      "",
+      "blarg"
+      };
+      String[] pageThree = {
+      "Page Three",
+      "z"
+      };
+      String[][] pageArr = {pageOne, pageTwo, pageThree};
+      return pageArr;
+   }
+}
