c4c25ccb613eabf039cc9c047625f879c9f85138
Michael <MichaelWidler@yahoo.com>
2023-09-15 06:14:19 -0700

    Create turret AI.

---
+++ Pulsar/AI/TurretAI.java
@@ -0,0 +1,34 @@
+package Pulsar.AI;
+
+import Pulsar.Actor.*;
+import Pulsar.Engine.*;
+
+public class TurretAI extends BasicAI
+{
+   public TurretAI(Actor s)
+   {
+      super(s);
+      team = Team.PLAYER;
+      self.setAlertness(Alertness.ALERT);
+   }
+   
+   public void plan()
+   {
+      Actor target = getNearestVisibleEnemy();
+      // no visible enemy
+      if(target != null)
+      {
+         // weapon is charged
+         if(canAttack() && GameEngine.hasClearShot(self, target))
+         {
+            setPendingTarget(target.getMapLoc());
+            setPendingAction(ActorAction.ATTACK);
+            return;
+         }
+      }
+      // default
+      setPendingTarget(self.getMapLoc());
+      setPendingAction(ActorAction.DELAY);
+
+   }
+}
