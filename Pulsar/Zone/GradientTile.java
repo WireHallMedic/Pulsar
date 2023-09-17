27f5ac1cc54ae115a65f6fa598ee5c20210ae903
Michael <MichaelWidler@yahoo.com>
2023-09-16 18:38:30 -0700

    GradientTiles can pulse foreground or background.

--- Pulsar/Zone/GradientTile.java
+++ Pulsar/Zone/GradientTile.java
@@ -9,36 +9,68 @@ public class GradientTile extends MapTile implements ZoneConstants, GUIConstants
    public static final int SLOW = 1;
    public static final int MEDIUM = 2;
    public static final int FAST = 3;
+   public static final boolean FOREGROUND = true;
+   public static final boolean BACKGROUND = false;
    
    private Color[] gradient;
    private int pulseSpeed;
+   private boolean pulseType;
    
    public Color[] getGradient(){return gradient;}
    public int getPulseSpeed(){return pulseSpeed;}
+   public boolean getPulseType(){return pulseType;}
    
    public void setGradient(Color[] g){gradient = g;}
    public void setPulseSpeed(int ps){pulseSpeed = ps;}
+   public void setPulseType(boolean pt){pulseType = pt;}
    
    public GradientTile(int i, int bg, String n, boolean lp, boolean hp, boolean t)
    {   
       super(i, DEFAULT_TILE_FG_COLOR.getRGB(), bg, n, lp, hp, t);
       gradient = null;
       pulseSpeed = MEDIUM;
+      pulseType = FOREGROUND;
    }
    
    @Override
    public int getFGColor()
    {
-      TileAnimationManager animationManager = GUITools.getAnimationManager();
-      if(gradient != null && animationManager != null)
+      if(pulseType == FOREGROUND)
       {
-         switch(pulseSpeed)
+         TileAnimationManager animationManager = GUITools.getAnimationManager();
+         if(gradient != null && animationManager != null)
          {
-            case SLOW:   return gradient[animationManager.slowPulse()].getRGB();
-            case MEDIUM: return gradient[animationManager.mediumPulse()].getRGB();
-            case FAST:   return gradient[animationManager.fastPulse()].getRGB();
+            switch(pulseSpeed)
+            {
+               case SLOW:   return gradient[animationManager.slowPulse()].getRGB();
+               case MEDIUM: return gradient[animationManager.mediumPulse()].getRGB();
+               case FAST:   return gradient[animationManager.fastPulse()].getRGB();
+            }
          }
+         return DEFAULT_TILE_FG_COLOR.getRGB();
       }
-      return DEFAULT_TILE_FG_COLOR.getRGB();
+      else
+         return super.getFGColor();
+   }
+   
+   @Override
+   public int getBGColor()
+   {
+      if(pulseType == BACKGROUND)
+      {
+         TileAnimationManager animationManager = GUITools.getAnimationManager();
+         if(gradient != null && animationManager != null)
+         {
+            switch(pulseSpeed)
+            {
+               case SLOW:   return gradient[animationManager.slowPulse()].getRGB();
+               case MEDIUM: return gradient[animationManager.mediumPulse()].getRGB();
+               case FAST:   return gradient[animationManager.fastPulse()].getRGB();
+            }
+         }
+         return DEFAULT_TILE_BG_COLOR.getRGB();
+      }
+      else
+         return super.getBGColor();
    }
 }
