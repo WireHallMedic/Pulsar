b9579f563c9e3b122235b8a834d71b60bc3579f2
Michael <MichaelWidler@yahoo.com>
2023-09-25 08:26:28 -0700

    Add tests for rotating and mirroring ObstacleTemplates.

--- Pulsar/Zone/ObstacleTemplateTest.java
+++ Pulsar/Zone/ObstacleTemplateTest.java
@@ -4,20 +4,65 @@ import org.junit.Assert;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
+import Pulsar.Engine.*;
+import WidlerSuite.*;;
 
 
-public class ObstacleTemplateTest {
+public class ObstacleTemplateTest implements ZoneConstants
+{
 
 
    /** Fixture initialization (common initialization
     *  for all tests). **/
-   @Before public void setUp() {
+   @Before public void setUp() 
+   {
+   
+   }
+      
+   public ObstacleTemplate getButtonObstacle()
+   {
+      String[] strArr = {"#...#",
+                         "..!..",
+                         ".....",
+                         ".....",
+                         "#...#"};
+      Vector<String> v = new Vector<String>();
+      for(int i = 0; i < strArr.length; i++)
+         v.add(strArr[i]);
+      ObstacleTemplate template = new ObstacleTemplate(v);
+      ButtonTrigger trigger = new ButtonTrigger();
+      trigger.parse("BUTTON_ACTION: FLOOD_WATER");
+      trigger.parse("BUTTON_TARGET: [3, 3]");
+      trigger.setCallerLoc(new Coord(2, 1));
+      template.setButtonTrigger(trigger);
+      return template;
+   }
+      
+   public ObstacleTemplate getBadObstacle()
+   {
+      String[] strArr = {"#...#",
+                         ".....",
+                         "#...#"};
+      Vector<String> v = new Vector<String>();
+      for(int i = 0; i < strArr.length; i++)
+         v.add(strArr[i]);
+      return new ObstacleTemplate(v);
+   }
+      
+   @Test public void testValidation()
+   {
+      ObstacleTemplate badTemplate = getBadObstacle();
+      assertThrows(java.lang.Error.class, () -> {badTemplate.validate();});
+      ObstacleTemplate template = getButtonObstacle();
+      assertTrue("RoomTemplate passes button validation without error.", template.validateButtons());
+      template.rotate();
+      assertTrue("RoomTemplate passes button validation without error after rotation.", template.validateButtons());
+      template.mirrorY();
+      assertTrue("RoomTemplate passes button validation without error after mirrorY.", template.validateButtons());
+      template.mirrorX();
+      assertTrue("RoomTemplate passes button validation without error after mirrorX.", template.validateButtons());
    }
 
-
-   /** A test that always fails. **/
-   @Test public void defaultTest() {
-      Assert.assertEquals("Default test added by jGRASP. Delete "
-            + "this test once you have added your own.", 0, 1);
-   }
+   
 }
