81760470ccec10fe8ad99a849050a031030a43c3
Michael <MichaelWidler@yahoo.com>
2023-09-25 05:39:49 -0700

    Obstacle Templates can have ButtonTriggers.

--- Pulsar/Zone/ObstacleTemplate.java
+++ Pulsar/Zone/ObstacleTemplate.java
@@ -1,21 +1,34 @@
 package Pulsar.Zone;
 
 import java.util.*;
+import Pulsar.Engine.*;
 
 public class ObstacleTemplate extends MapTemplate implements ZoneConstants
 {
    public static final int REQUIRED_WIDTH = 5;
    public static final int REQUIRED_HEIGHT = 5;
    
+   private ButtonTrigger buttonTrigger;
+   
+   public void setButtonTrigger(ButtonTrigger t){buttonTrigger = t;}
+
+   public ButtonTrigger getButtonTrigger(){return buttonTrigger;}
+   public boolean hasButtonTrigger(){return buttonTrigger != null;}
+   
    public ObstacleTemplate(Vector<String> input)
    {
       super(input);
+      buttonTrigger = null;
       validate();
    }
    
    public ObstacleTemplate(ObstacleTemplate that)
    {
       super(that);
+      if(that.hasButtonTrigger())
+      {
+         this.buttonTrigger = new ButtonTrigger(that.buttonTrigger);
+      }
    }
       
    public void print()
