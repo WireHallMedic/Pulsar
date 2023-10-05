14655d1120fe681844d5cb10040457596a8c7600
Michael <MichaelWidler@yahoo.com>
2023-10-05 07:47:12 -0700

    Add loot roll that exclused credits.

--- Pulsar/Gear/LootFactory.java
+++ Pulsar/Gear/LootFactory.java
@@ -1,6 +1,7 @@
 package Pulsar.Gear;
 
 import Pulsar.Engine.*;
+import java.util.*;
 
 public class LootFactory implements GearConstants
 {
@@ -10,12 +11,42 @@ public class LootFactory implements GearConstants
       return (LootType)table.roll();
    }
    
+   public static LootType rollNonCreditLootType()
+   {
+      LootType[] typeList = new LootType[LootType.values().length - 1];
+      int i = 0;
+      for(LootType type : LootType.values())
+      {
+         if(type == LootType.CREDITS)
+            continue;
+         typeList[i] = type;
+         i++;
+      }
+      WeightedRandomizer table = new WeightedRandomizer(typeList);
+      return (LootType)table.roll();
+   }
+   
+   public static LootRarity rollLootRarity()
+   {
+      WeightedRandomizer table = new WeightedRandomizer(LootRarity.values());
+      return (LootRarity)table.roll();
+   }
+   
    public static void main(String[] args)
    {
       int maxReps = 10000;
       int[] results = new int[LootType.values().length];
       for(int i = 0; i < maxReps; i++)
          results[rollLootType().ordinal()]++;
+      for(int i = 0; i < LootType.values().length; i++)
+      {
+         double percent = results[i] * 100.0 / maxReps;
+         System.out.println(LootType.values()[i] + ": " + percent + "%");
+      }
+      System.out.println("Non-Credit Rolls:");
+      results = new int[LootType.values().length];
+      for(int i = 0; i < maxReps; i++)
+         results[rollNonCreditLootType().ordinal()]++;
       for(int i = 0; i < LootType.values().length; i++)
       {
          double percent = results[i] * 100.0 / maxReps;
