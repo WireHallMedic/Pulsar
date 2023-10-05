b8c6330ecfebaf34a1c74ccaa3776bad896ab309
Michael <MichaelWidler@yahoo.com>
2023-10-05 11:06:59 -0700

    Add basic armor.

--- Pulsar/Gear/ArmorFactory.java
+++ Pulsar/Gear/ArmorFactory.java
@@ -7,6 +7,24 @@ public class ArmorFactory implements GearConstants, ActorConstants
 {
    public enum ArmorType implements WeightedRandomizable
    {
+      BASIC_ARMOR    (30)
+      SCOUT_ARMOR    (10),
+      ENGINEER_ARMOR (10),
+      ASSAULT_ARMOR  (10);
+      
+      private ArmorType(int w)
+      {
+         weight = w;
+      }
+      
+      private int weight;
+      
+      public int getWeight(){return weight;}
+   }
+   
+   public enum ArmorType implements WeightedRandomizable
+   {
+      BASIC_ARMOR    (30)
       SCOUT_ARMOR    (10),
       ENGINEER_ARMOR (10),
       ASSAULT_ARMOR  (10);
@@ -33,6 +51,7 @@ public class ArmorFactory implements GearConstants, ActorConstants
       ArmorType type = (ArmorType)table.roll();
       switch(type)
       {
+         case BASIC_ARMOR     : return getBasicArmor();
          case SCOUT_ARMOR     : return getScoutArmor();
          case ENGINEER_ARMOR  : return getEngineerArmor();
          case ASSAULT_ARMOR   : return getAssaultArmor();
@@ -40,29 +59,35 @@ public class ArmorFactory implements GearConstants, ActorConstants
       }
    }
    
-   public static Armor getScoutArmor()
+   public static Armor getBasicArmor()
    {
       Armor a = new Armor();
+      a.setName("Basic Armor");
+      a.setDamageReduction(2);
+	   a.setSpeedCap(ActionSpeed.FAST);
+      return a;
+   }
+   
+   public static Armor getScoutArmor()
+   {
+      Armor a = getBasicArmor();
       a.setName("Scout Armor");
       a.setDamageReduction(0);
-	   a.setSpeedCap(ActionSpeed.FAST);
       a.setGadgetEffect(GadgetSpecialEffect.MOTION_SENSOR);
       return a;
    }
    
    public static Armor getEngineerArmor()
    {
-      Armor a = new Armor();
+      Armor a = getBasicArmor();
       a.setName("Engineer Armor");
-      a.setDamageReduction(2);
-	   a.setSpeedCap(ActionSpeed.FAST);
       a.setInventorySpace(2);
       return a;
    }
    
    public static Armor getAssaultArmor()
    {
-      Armor a = new Armor();
+      Armor a = getBasicArmor();
       a.setName("Assault Armor");
       a.setDamageReduction(4);
 	   a.setSpeedCap(ActionSpeed.STANDARD);
@@ -72,7 +97,9 @@ public class ArmorFactory implements GearConstants, ActorConstants
    
    public static void main(String[] args)
    {
-      Armor a = getScoutArmor();
+      Armor a = getBasicArmor();
+      System.out.println(a.getName() + ": " + a.getSummary());
+      a = getScoutArmor();
       System.out.println(a.getName() + ": " + a.getSummary());
       a = getEngineerArmor();
       System.out.println(a.getName() + ": " + a.getSummary());
